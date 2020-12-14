42c8b9d14e8095c5edb169f84fa09cc12442ef98
Colin Seymour <colin@symr.io>
2020-12-14 09:45:04 +0000

    Add initial functionality

---
+++ lib/jekyll-image-cache.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "jekyll"
+require "nokogiri"
+
+module Jekyll
+  class ImageCache
+    def self.process(content)
+      html = content.output
+      content.output = process_tags(html) if process_tags?(html)
+    end
+
+    def self.process?(doc)
+      (doc.is_a?(Jekyll::Page) || doc.write?) && doc.output_ext == ".html" ||
+        doc.permalink&.end_with?("/")
+    end
+
+    def self.process_tags?(html)
+      html.include?("<img")
+    end
+
+    def self.process_tags(html)
+      content = Nokogiri.HTML(html)
+      # TODO: Make the tag configurable
+      tags = content.css("img[src].u-photo")
+      # TODO: Make the cache service and size configurable
+      tags.each do |tag|
+        orig_url = tag["src"].sub(%r!https?://!, "")
+        unless orig_url.include? "images.weserv.nl"
+          tag["src"] = "//images.weserv.nl/?url=#{orig_url}&w=640"
+        end
+      end
+      content.to_html
+    end
+
+    private_class_method :process_tags
+    private_class_method :process_tags?
+  end
+end
+
+Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
+  Jekyll::ImageCache.process(doc) if Jekyll::ImageCache.process?(doc)
+end
---
+++ lib/jekyll-image-cache/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class ImageCache
+    VERSION = "0.0.1"
+  end
+end
